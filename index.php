f7721fd7b4c249b1a0f7718f8bdce78a097f6fbf
Vinícius Oliveira <vinicius.oliveira1911@gmail.com>
2021-03-17 19:07:56 -0400

    Update

--- index.php
+++ index.php
@@ -14,23 +14,24 @@
                 <a href="consultas.php"><li>Consultas</li></a>
             </ul>
         </nav>
+        
         <section>
             <h1>Cadastro de Usuários</h1>
             <hr><br><br>
-
             <form method="post" action="processa.php">
 
                 Nome<br>
-                <input type="text" name="nome" class="campo" maxlength="40" required autofocus> <br>
+                <input type="text" name="nome" class="campo" placeholder="Nome completo" maxlength="40" required autofocus> <br>
                 E-mail<br>
-                <input type="email" name="email" class="campo" maxlength="50" required> <br>
+                <input type="email" name="email" class="campo" placeholder="Digite seu e-mail" maxlength="50" required> <br>
                 Curso<br>
-                <input type="text" name="curso" class="campo" maxlength="40" required> <br>
+                <input type="text" name="curso" class="campo" placeholder="Ex:Engenharia" maxlength="40" required> <br>
                 <br><br>
 
-                <input type="submit" value="Salvar" class="btn">
+                <input type="submit" value="Salvar"class="btn">
                 <input type="reset" value="Limpar" class="btn">
             </form>
+            <img src="assets/img.jpg" alt="Usúario" class="imgbody">
         </section>
     </div>
 </body>
